6b6a4c0a2a35d798ddcb9b035bcc57f5011afb57
tschertenleib <till.schertenleib@gmail.com>
2026-03-30 16:00:36 +0200

    fixing __init__.py file

--- src/diffpy/adpdf/__init__.py
+++ src/diffpy/adpdf/__init__.py
@@ -12,7 +12,7 @@
 # See LICENSE.rst for license information.
 #
 ##############################################################################
-""""description""""
+"""description."""
 
 # package version
 from diffpy.adpdf.version import __version__
